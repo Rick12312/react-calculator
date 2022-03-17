80f26c254fc7d335511d1374e49cffcf4f69e4aa
Rick <rick@Ricks-MacBook-Air.local>
2022-03-17 19:46:38 +0000

    Digit Button and Operation Button components updated

---
+++ react-calculator/src/OperationButton.js
@@ -0,0 +1,15 @@
+import { ACTIONS } from "./App";
+
+function DigitButton({ dispatch, operation }) {
+  return (
+    <button
+      onClick={() =>
+        dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
+      }
+    >
+      {operation}
+    </button>
+  );
+}
+
+export default DigitButton;
